a1f793718ad9636a1130410d7b82ec0b68e746e0
radjab <radjab@gmail.com>
2019-02-08 05:34:42 +0400

    conditional expression is added returns value only if condition is satisfied

--- src/main/java/com/linkedlogics/bio/expression/Conditional.java
+++ src/main/java/com/linkedlogics/bio/expression/Conditional.java
@@ -1,5 +1,29 @@
 package com.linkedlogics.bio.expression;
 
-public class Conditional {
+import com.linkedlogics.bio.BioObject;
 
+public class Conditional extends Expression {
+	private Expression value ;
+	private Expression elseValue ;
+	private Expression condition ;
+	
+	public Conditional(Expression condition, Expression value) {
+		this(condition, value, null) ;
+	}
+	
+	public Conditional(Expression condition, Expression value, Expression elseValue) {
+		this.condition = condition ;
+		this.value = value ;
+		this.elseValue = elseValue ;
+	}
+	
+	@Override
+	protected Object getValue(Object source, BioObject... params) {
+		if (condition.getBooleanValue(params)) {
+			return value.getValue(params) ;
+		} else if (elseValue != null) {
+			return elseValue.getValue(params) ;
+		}
+		return null;
+	}
 }
