a685c899253085f8ca02e1311fce3459185899dd
radjab <radjab@gmail.com>
2019-02-12 15:23:38 +0400

    module changes for bio-sql-logic project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,10 +8,12 @@ module bio.object {
 	exports com.linkedlogics.bio.annotation;
 	exports com.linkedlogics.bio.parser;
 	exports com.linkedlogics.bio.dictionary to bio.sql ;
+	exports com.linkedlogics.bio.expression to bio.sql ;
 	exports com.linkedlogics.bio.dictionary.builder to bio.sql ;
+	exports com.linkedlogics.bio.utility to bio.sql ;
 	
 	requires transitive java.xml;
 	requires transitive org.json;
-	requires io.github.classgraph ;
-	requires lz4;
+	requires transitive io.github.classgraph ;
+	requires transitive lz4;
 }
