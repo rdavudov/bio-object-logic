40f0179fc7483ca2d3a40a5658019d356babc8c6
radjab <radjab@gmail.com>
2019-04-04 11:45:20 +0400

    ability to refer to caller value using self parameter

--- src/main/java/com/linkedlogics/bio/expression/GenericFunction.java
+++ src/main/java/com/linkedlogics/bio/expression/GenericFunction.java
@@ -3,7 +3,11 @@ package com.linkedlogics.bio.expression;
 import com.linkedlogics.bio.BioExpression;
 import com.linkedlogics.bio.BioFunction;
 import com.linkedlogics.bio.BioObject;
-
+/**
+ * Generic function is used inside xml configuration where you define your function as a bio expression
+ * @author rajab
+ *
+ */
 public class GenericFunction implements BioFunction {
 	private BioExpression expression ;
 	private String[] parameterNames ;
@@ -23,9 +27,13 @@ public class GenericFunction implements BioFunction {
 
 	@Override
 	public Object getValue(Object value, Object... parameters) {
+		// since our expression function uses parameters we need to populate their values into a bio object instance
 		BioObject input = new BioObject() ; 
-		for (int i = 0; i < parameters.length; i++) {
+		for (int i = 0; i < parameters.length && i < parameterNames.length; i++) {
 			input.put(parameterNames[i], parameters[i]) ;
+		}
+		if (value != null) {
+			input.put("self", value) ;
 		}
 		
 		return expression.getValue(input);
