4d0fd7f523416d0bf9c463698db81f916fd89804
radjab <radjab@gmail.com>
2019-04-22 14:59:22 +0400

    pom change

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,11 +7,11 @@ module com.linkedlogics.bio {
 	exports com.linkedlogics.bio.exception;
 	exports com.linkedlogics.bio.annotation;
 	exports com.linkedlogics.bio.parser;
+	exports com.linkedlogics.bio.utility;
 	exports com.linkedlogics.bio.dictionary to com.linkedlogics.bio.sql ;
 	exports com.linkedlogics.bio.expression to com.linkedlogics.bio.sql ;
 	exports com.linkedlogics.bio.dictionary.builder to com.linkedlogics.bio.sql ;
-	exports com.linkedlogics.bio.utility to com.linkedlogics.bio.sql, com.linkedlogics.processor ;
-	exports com.linkedlogics.bio.stream to com.linkedlogics.processor ;
+	exports com.linkedlogics.bio.stream to com.linkedlogics.processor, com.linkedlogics ;
 	
 	requires transitive java.xml;
 	requires transitive org.json;
